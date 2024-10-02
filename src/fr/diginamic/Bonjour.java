0d3be0a1574ed4d7db2feb4eb8f388cdefa4660e
parameswaran <prebou1999@gmail.com>
2024-10-02 11:52:08 +0200

    version 1

---
+++ src/fr/diginamic/Bonjour.java
@@ -0,0 +1,9 @@
+package fr.diginamic;
+
+import java.sql.SQLOutput;
+
+public class Bonjour {
+    public static void main(String arg[]){
+        System.out.println("Bonjour tout le monde");
+    }
+}
